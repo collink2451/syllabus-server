af3ba91a1d4487327df5999ea9dd28b5a2f994bf
Collin Koldoff <collinkoldoff@gmail.com>
2024-04-04 17:38:18 -0500

    Added Models index

---
+++ src/models/index.ts
@@ -0,0 +1,5 @@
+import ClassModel from "./ClassModel";
+import ClassSectionModel from "./ClassSectionModel";
+import InstructorModel from "./InstructorModel";
+
+export { ClassModel, ClassSectionModel, InstructorModel };
